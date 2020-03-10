d8379eb2beef2d8aab2ba294b76bce44dfef72c2
milanovic978 <61240063+milanovic978@users.noreply.github.com>
2020-03-10 17:04:15 +0100

    Playing field generator

--- LineInObstacle.m
+++ LineInObstacle.m
@@ -1,7 +1,9 @@
 function [coll]=LineInObstacle(point1,point2,shape)
-xx=linspace(point1(1),point2(1),100);
-yy=linspace(point1(2),point2(2),100);
-zz=linspace(point1(3),point2(3),100);
-tf = inShape(shape,xx,yy,zz);
+n=100;  %number of points on line
+%Create points on the line
+xx=linspace(point1(1),point2(1),n);
+yy=linspace(point1(2),point2(2),n);
+zz=linspace(point1(3),point2(3),n);
+tf = inShape(shape,xx,yy,zz); %check if points collide with obstacle
 coll=any(tf);
 end
