d1a89465c097e99e57fb6d9c1adb297c91452b35
esalzmann <e.h.salzmann@student.tudelft.nl>
2020-03-11 13:18:45 +0100

    Included ball radius
    Doesn't work accordingly

--- main.m
+++ main.m
@@ -47,17 +47,21 @@ for i=2:N+1
 
     % check if q_new is in collision
     
-    % compute radius with q_new as center
-    ball_radius = gamma * nthroot(log(i-1)/(i-1),dim);
+
     
 %     original
-%     [~, I] = min(nodeDist(q_new, nodes));
-%     edges(:, i) = [I, i]; % "connection via indices"
-%     nodes(:, i) = q_new;
-    q_near = rangesearch(q_new, nodes, ball_radius);
-    [~, I] = min(nodeDist(q_new, q_near));
+    [~, I] = min(nodeDist(q_new, nodes));
     edges(:, i) = [I, i]; % "connection via indices"
     nodes(:, i) = q_new;
+
+    % Optimal RRT
+%     n = nnz(nodes);
+%     ball_radius = gamma * nthroot(log(n)/(n),dim);
+%     q_near = rangesearch([nodes(1) nodes(2)], [q_new(1) q_new(2)], ball_radius);
+%     q_near = str2double (q_near);
+%     [~, I] = min(nodeDist(q_new, q_near));
+%     edges(:, i) = [I, i]; % "connection via indices"
+%     nodes(:, i) = q_new;
 end
 
 for i=2:N+1
