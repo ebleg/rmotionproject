01460bc9605d6a13042056d445d96fda2f999002
milanovic978 <61240063+milanovic978@users.noreply.github.com>
2020-03-17 18:17:23 +0100

    Revert "Revert "Trying to solve the inShape function for all shapes""
    This reverts commit 1cd422865b12b1de4e00f74a61d4dc2db6adf126.

--- findNearNodes.m
+++ findNearNodes.m
@@ -1,7 +1,16 @@
-function [nodes_near]=findNearNodes(nodes, q_new, dim, gamma)
-n = nnz(nodes);
+function [nodes_near]=findNearNodes(nodes, q_new, dim, gamma, nodeDist)
+n = nnz(nodes)/dim;
 ball_radius = gamma * nthroot(log(n)/(n),dim);
-[indices,dists] = findNeighborsInRadius(ptCloud,point,radius);
+I = NaN(length(n));
+for i = 1:length(n)
+    Dist = nodeDist(nodes, q_new);
+    if Dist <= ball_radius
+        [~, I] = [Dist, indices];
+    end
+    [nodes_near] = I;
+end
+
+% [indices,dists] = findNeighborsInRadius(nodes,q_new,ball_radius);
 % q_near = rangesearch([nodes(1) nodes(2)], [q_new(1) q_new(2)], ball_radius);
 % q_near = str2double (q_near);
 % [~, I] = min(nodeDist(q_new, q_near));
